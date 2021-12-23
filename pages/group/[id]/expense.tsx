ee75b714b0486e0d38c04d4297ef43267174e590
Farid <fnikman@gmail.com>
2021-12-23 16:15:17 +0800

    fix: expense layout

--- pages/group/[id]/expense.tsx
+++ pages/group/[id]/expense.tsx
@@ -1,8 +1,16 @@
 import { FunctionComponent } from "react";
 import { Container } from "../../../src/components/Container";
+import { ExpenseForm } from "../../../src/modules/Form/Expense";
 
 const Expense: FunctionComponent = () => {
-  return <Container>hello add expense</Container>;
+  return (
+    <Container>
+      <h1 className="mt-20 mb-8 text-2xl font-extrabold text-gray-700 md:max-w-4xl sm:text-3xl">
+        Add Expense
+      </h1>
+      <ExpenseForm />
+    </Container>
+  );
 };
 
 export default Expense;
