201b6e0dd35a1035ca613f50cd46f7fad90f7910
Farid <fnikman@gmail.com>
2021-12-27 14:51:08 +0800

    fix: include handle delete

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,9 +1,16 @@
 import { FunctionComponent } from "react";
 
-export const Chip: FunctionComponent = props => (
+interface IChip {
+  handleDelete?: () => void;
+}
+
+export const Chip: FunctionComponent<IChip> = props => (
   <div className=" border-2 mx-0.5 flex items-center border-gray-300 rounded-xl">
     <span className="px-2 text-gray-700 ">{props.children}</span>
-    <button className="mr-1 block text-center hover:bg-gray-400 bg-gray-300 text-xs rounded-2xl w-4 h-4">
+    <button
+      onClick={props?.handleDelete}
+      className="mr-1 block text-center hover:bg-gray-400 bg-gray-300 text-xs rounded-2xl w-4 h-4"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-3 w-3 m-auto text-white "
