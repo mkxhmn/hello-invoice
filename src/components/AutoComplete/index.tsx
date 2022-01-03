e67f0a4b2bd4179a86cd96c3c388a9ff6b21e763
Farid <fnikman@gmail.com>
2022-01-03 15:01:31 +0800

    feat: autocomplete interaction layout

--- src/components/AutoComplete/index.tsx
+++ src/components/AutoComplete/index.tsx
@@ -1,6 +1,7 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { ITextField, TextField } from "../TextField";
 import { IModal, Modal } from "./Modal.AutoComplete";
+import { shift, useFloating } from "@floating-ui/react-dom";
 
 interface IAutoComplete extends ITextField, IModal {}
 
@@ -10,11 +11,38 @@ export const AutoComplete: FunctionComponent<IAutoComplete> = ({
   selections,
   ...textFieldProps
 }) => {
-  return (
-    <div>
-      <TextField label={label} errors={errors} {...textFieldProps} />
+  const { x, y, reference, floating, strategy, refs } = useFloating({
+    placement: "bottom-start",
+    middleware: [shift()]
+  });
 
-      <Modal selections={selections} />
+  const [showModal, setShowModal] = useState(() => false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div ref={reference} className=" relative ">
+      <TextField
+        onFocus={handleShowModal}
+        label={label}
+        errors={errors}
+        {...textFieldProps}
+      />
+      {showModal && (
+        <div
+          className="w-full"
+          ref={floating}
+          style={{
+            left: x ?? undefined,
+            top: y ?? undefined,
+            position: strategy
+          }}
+        >
+          <Modal selections={selections} show={showModal} />
+        </div>
+      )}
     </div>
   );
 };
