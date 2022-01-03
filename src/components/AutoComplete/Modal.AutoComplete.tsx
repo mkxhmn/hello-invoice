64ae6382650da63f8efcbd9e96103a08c7f4d0f6
Farid <fnikman@gmail.com>
2022-01-03 15:15:57 +0800

    feat: combine parent div in one component

--- src/components/AutoComplete/Modal.AutoComplete.tsx
+++ src/components/AutoComplete/Modal.AutoComplete.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties, Ref } from "react";
+
 export interface ISelections {
   label: string;
   value: number;
@@ -6,11 +8,21 @@ export interface ISelections {
 export interface IModal {
   selections: ISelections[];
   show: boolean;
+  modalStyle: CSSProperties;
+  modalRef: Ref<HTMLDivElement>;
 }
 
-export function Modal({ selections, show }: IModal) {
+export function Modal({ selections, show, modalStyle, modalRef }: IModal) {
+  if (!show) {
+    return null;
+  }
+
   return (
-    <div className=" py-2 mt-1 border-gray-50 bg-white border-2 rounded-lg shadow-md w-full ">
+    <div
+      className=" py-2 mt-1 border-gray-50 bg-white border-2 rounded-lg shadow-md w-full "
+      style={modalStyle}
+      ref={modalRef}
+    >
       {selections.map(({ value, label }) => (
         <label key={value} className="flex items-center py-2 pl-3 ">
           <input
