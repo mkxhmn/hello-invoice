470404d7cf1d289aaa36d984614697bdaab54d3d
Farid <fnikman@gmail.com>
2022-01-12 11:22:12 +0800

    feat: handleRouteToGroup

--- src/components/GroupCard/Summary.tsx
+++ src/components/GroupCard/Summary.tsx
@@ -1,10 +1,20 @@
 import { IGroup } from "../../store/model/group";
 import { UserAvatarGroup } from "../UserAvatarGroup";
+import { useRouter } from "next/router";
 
 export const Summary = (group: IGroup) => {
+  const router = useRouter();
+
+  const handleRouteToGroup = async () => {
+    await router.push("/group/" + group.id);
+  };
+
   return (
-    <div className="border-2 border-black pt-1 pb-3 px-1">
-      <h2>{group.name}</h2>
+    <div
+      onClick={handleRouteToGroup}
+      className="border-2 border-black pt-1 pb-3 px-1 cursor-pointer"
+    >
+      <h3>{group.name}</h3>
       <UserAvatarGroup users={group.users} />
     </div>
   );
