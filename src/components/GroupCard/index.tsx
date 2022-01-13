4e477f15b768c86eb97034e9a68296cc7c28eb02
Farid <fnikman@gmail.com>
2022-01-13 15:02:39 +0800

    feat: motion GroupCard

--- src/components/GroupCard/index.tsx
+++ src/components/GroupCard/index.tsx
@@ -1,19 +1,52 @@
 import { useStoreState } from "../../store/hooks";
 import dynamic from "next/dynamic";
 import { IGroup } from "../../store/model/group";
+import { motion } from "framer-motion";
 
 const Summary = dynamic<IGroup>(() =>
   import("./Summary").then(component => component.Summary)
 );
 
+const container = {
+  visible: {
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1
+    }
+  },
+  hidden: {
+    transition: {
+      when: "afterChildren"
+    }
+  }
+};
+
+const card = {
+  visible: {
+    x: 0,
+    opacity: 1
+  },
+  hidden: {
+    x: -32,
+    opacity: 0.4
+  }
+};
+
 export const GroupCard = () => {
   const groups = useStoreState(state => state.group.groups);
 
   return (
-    <div className="mt-12 grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4">
+    <motion.div
+      variants={container}
+      animate="visible"
+      initial="hidden"
+      className="mt-12 grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4"
+    >
       {groups.map(group => (
-        <Summary {...group} key={group.id} />
+        <motion.div key={group.id} variants={card}>
+          <Summary {...group} key={group.id} />
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
