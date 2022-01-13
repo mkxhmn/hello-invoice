f5bc930fca0e1b90462db16cd7ba7c22a28c5c0f
Farid <fnikman@gmail.com>
2022-01-13 16:46:11 +0800

    feat: create group refactor

--- pages/group/create.tsx
+++ pages/group/create.tsx
@@ -1,12 +1,13 @@
 import { Container } from "../../src/components/Container";
+import { Title } from "../../src/components/Title";
 import { GroupForm } from "../../src/modules/Form/Group";
 
 const CreateGroup = () => {
   return (
     <Container>
-      <h1 className="mt-20 mb-8 text-2xl font-extrabold text-gray-700 md:max-w-4xl sm:text-3xl">
-        create group
-      </h1>
+      <div className="mt-20 mb-8">
+        <Title>Create Group</Title>
+      </div>
       <GroupForm />
     </Container>
   );
