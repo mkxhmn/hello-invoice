2574a9c2ee31e646a4653e5f5606699b3532c096
Farid <fnikman@gmail.com>
2022-01-13 15:15:53 +0800

    feat: title layout

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -13,7 +13,7 @@ export const Title: FunctionComponent = ({ children }) => (
       stiffness: 420,
       damping: 20
     }}
-    className="mt-20 mb-8 text-2xl font-extrabold text-gray-800 md:max-w-4xl sm:text-3xl"
+    className="text-2xl font-extrabold text-gray-800 md:max-w-4xl sm:text-3xl"
   >
     {children}
   </motion.h1>
